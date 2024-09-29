7a3de924af89c91aa59137e51ca5f7c48e126eff
hungrytech <xorals9448@gmail.com>
2024-09-29 21:55:11 +0900

    Remove dependencyResolutionManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,3 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "chzzk-with-me"
-
-// dependencyResolutionManagement {
-//    versionCatalogs {
-//        create("libs") {
-//            from("gradle/libs.versions.toml")
-//        }
-//    }
-// }
